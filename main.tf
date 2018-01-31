b1efde596b40f63ec6152ca47fbc9c4df935acbe
leszekg <gonczar@gmail.com>
2018-01-31 11:23:18 +0000

    Change the module to use Azure Resource Manager template
    The Hashicorp resources don't support shared access keys and many other things.

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-resource "azurerm_servicebus_topic" "topic" {
-  name                = "${var.name}"
-  resource_group_name = "${var.resource_group_name}"
-  namespace_name      = "${var.namespace_name}"
+# ARM template for Service Bus topic
+data "template_file" "topic_template" {
+  template = "${file("${path.module}/template/topic_template.json")}"
+}
 
-  enable_partitioning = true
+# Create Azure Service Bus topic
+resource "azurerm_template_deployment" "topic" {
+  template_body       = "${data.template_file.topic_template.rendered}"
+  name                = "${var.name}"
+  deployment_mode     = "Incremental"
+  resource_group_name = "${var.resource_group_name}"
+
+  parameters = {
+    serviceBusNamespaceName     = "${var.namespace_name}"
+    serviceBusTopicName         = "${var.name}"
+  }
 }
