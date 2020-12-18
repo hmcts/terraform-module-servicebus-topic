b0d5d8831eee1afdeb634c55b817452ffbe25b23
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-12-18 10:25:43 +0000

    Upgrade TF repo

--- main.tf
+++ main.tf
@@ -1,17 +1,29 @@
-# ARM template for Service Bus topic
-data "template_file" "topic_template" {
-  template = "${file("${path.module}/template/topic_template.json")}"
+locals {
+  auth_rule_name = "SendAndListenSharedAccessKey"
 }
 
-# Create Azure Service Bus topic
-resource "azurerm_template_deployment" "topic" {
-  template_body       = "${data.template_file.topic_template.rendered}"
-  name                = "${var.name}"
-  deployment_mode     = "Incremental"
-  resource_group_name = "${var.resource_group_name}"
+resource "azurerm_servicebus_topic" "servicebus_topic" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  namespace_name      = var.namespace_name
 
-  parameters = {
-    serviceBusNamespaceName     = "${var.namespace_name}"
-    serviceBusTopicName         = "${var.name}"
-  }
+  enable_partitioning                     = var.enable_partitioning
+  default_message_ttl                     = var.default_message_ttl
+  max_size_in_megabytes                   = var.max_size_in_megabytes
+  requires_duplicate_detection            = var.requires_duplicate_detection
+  duplicate_detection_history_time_window = var.duplicate_detection_history_time_window
+  enable_batched_operations               = var.enable_batched_operations
+  support_ordering                        = var.support_ordering
+  auto_delete_on_idle                     = var.auto_delete_on_idle
+  enable_express                          = var.enable_express
+}
+
+resource "azurerm_servicebus_topic_authorization_rule" "topic_authorization_rule" {
+  name                = local.auth_rule_name
+  namespace_name      = var.namespace_name
+  topic_name          = azurerm_servicebus_topic.servicebus_topic.name
+  resource_group_name = var.resource_group_name
+
+  listen = true
+  send   = true
 }
--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 # name of the topic
 output "name" {
-  value = "${azurerm_template_deployment.topic.outputs["topicName"]}"
+  value = azurerm_servicebus_topic.servicebus_topic.name
 }
 
 # primary connection string for send and listen operations
 output "primary_send_and_listen_connection_string" {
-  value = "${azurerm_template_deployment.topic.outputs["primarySendAndListenConnectionString"]}"
+  value = azurerm_servicebus_topic_authorization_rule.topic_authorization_rule.primary_connection_string
 }
 
 # secondary connection string for send and listen operations
 output "secondary_send_and_listen_connection_string" {
-  value = "${azurerm_template_deployment.topic.outputs["secondarySendAndListenConnectionString"]}"
+  value = azurerm_servicebus_topic_authorization_rule.topic_authorization_rule.secondary_connection_string
 }
 
 # primary shared access key with send and listen rights
 output "primary_send_and_listen_shared_access_key" {
-  value = "${azurerm_template_deployment.topic.outputs["primarySendAndListenSharedAccessKey"]}"
+  value = azurerm_servicebus_topic_authorization_rule.topic_authorization_rule.primary_key
 }
 
 # secondary shared access key with send and listen rights
 output "secondary_send_and_listen_shared_access_key" {
-  value = "${azurerm_template_deployment.topic.outputs["secondarySendAndListenSharedAccessKey"]}"
+  value = azurerm_servicebus_topic_authorization_rule.topic_authorization_rule.secondary_key
 }
