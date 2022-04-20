fa242ea6498017748ccf5404956b0c941eacd7e9
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-20 15:20:33 +0100

    DTSPO-6371 - stop using deprecated arguments

--- main.tf
+++ main.tf
@@ -2,10 +2,14 @@ locals {
   auth_rule_name = "SendAndListenSharedAccessKey"
 }
 
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_servicebus_topic" "servicebus_topic" {
   name                = var.name
-  resource_group_name = var.resource_group_name
-  namespace_name      = var.namespace_name
+  resource_group_name = data.azurerm_resource_group.id
+  namespace_id        = var.namespace_name
 
   enable_partitioning                     = var.enable_partitioning
   default_message_ttl                     = var.default_message_ttl
@@ -21,7 +25,7 @@ resource "azurerm_servicebus_topic" "servicebus_topic" {
 resource "azurerm_servicebus_topic_authorization_rule" "send_listen_auth_rule" {
   name                = local.auth_rule_name
   namespace_name      = var.namespace_name
-  topic_name          = azurerm_servicebus_topic.servicebus_topic.name
+  topic_id          = azurerm_servicebus_topic.servicebus_topic.id
   resource_group_name = var.resource_group_name
 
   listen = true
