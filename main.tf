ec84229d263ce28d91492b577608e0b616207edf
msl8r <72858053+msl8r@users.noreply.github.com>
2022-05-12 12:22:42 +0100

    DTSPO-6371 azurerm upgrade (#9)
    * DTSPO-6371 - stop using deprecated arguments
    
    * DTSPO-6371 - remove resource group name
    
    * DTSPO-6371 - remove deprecated args
    
    * DTSPO-6371 - remove deprecated args
    
    * DTSPO-6371 - fix rg id
    
    * Update main.tf
    
    * DTSPO-6371 - tf fmt
    
    * DTSPO-6371 - fix formatting
    
    * DTSPO-6371 - set min azurerm provider version
    
    * Update provider.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,19 @@ locals {
   auth_rule_name = "SendAndListenSharedAccessKey"
 }
 
-resource "azurerm_servicebus_topic" "servicebus_topic" {
-  name                = var.name
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
+}
+
+data "azurerm_servicebus_namespace" "this" {
+  name                = var.namespace_name
   resource_group_name = var.resource_group_name
-  namespace_name      = var.namespace_name
+}
+
+
+resource "azurerm_servicebus_topic" "servicebus_topic" {
+  name         = var.name
+  namespace_id = data.azurerm_servicebus_namespace.this.id
 
   enable_partitioning                     = var.enable_partitioning
   default_message_ttl                     = var.default_message_ttl
@@ -19,10 +28,8 @@ resource "azurerm_servicebus_topic" "servicebus_topic" {
 }
 
 resource "azurerm_servicebus_topic_authorization_rule" "send_listen_auth_rule" {
-  name                = local.auth_rule_name
-  namespace_name      = var.namespace_name
-  topic_name          = azurerm_servicebus_topic.servicebus_topic.name
-  resource_group_name = var.resource_group_name
+  name     = local.auth_rule_name
+  topic_id = azurerm_servicebus_topic.servicebus_topic.id
 
   listen = true
   send   = true
